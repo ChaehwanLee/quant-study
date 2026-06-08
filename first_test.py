6c997503b61b764cc2e1f1467d3289f58e63fdc8
Chaehwan <oasisinyourmind@gmail.com>
2026-06-09 01:23:27 +0900

    add: SK하이닉스 2026 주가 데이터 조회

--- first_test.py
+++ first_test.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import FinanceDataReader as fdr
 
-# 삼성전자 주가 데이터 가져오기
-df = fdr.DataReader('005930', '2024-01-01', '2024-12-31')
-print(df.head())
+df = fdr.DataReader('000660', '2026-01-01', '2026-06-09')
+print(df)
